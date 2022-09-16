4641c66ac4c947b4dc941486c71ffb54722edc86
viktor91 <v.a.taradaiko@gmail.com>
2022-09-16 13:35:05 +0400

    Error correction

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -1,19 +1,15 @@
 package ru.netology;
 
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.conditions.Text;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 import utils.DataGenerator;
 
-import java.time.LocalDate;
 
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 import static java.time.Duration.ofSeconds;
-import static java.time.format.DateTimeFormatter.ofPattern;
 import static utils.DataGenerator.Registration.generateDateOfMeeting;
 
 
@@ -33,24 +29,19 @@ public class CardDeliveryTest {
         $("[placeholder='Дата встречи']").setValue(firstMeeting);
         $("[data-test-id='name'] input").setValue(DataGenerator.Registration.generateClientPersonalData("ru").getFullName());
         $("[data-test-id='phone'] input").setValue(DataGenerator.Registration.generateClientPersonalData("ru").getPhoneNumber());
-        $x("//*[@id=\"root\"]/div/form/fieldset/div[5]").click();
+        $("[data-test-id=agreement] .checkbox__box").click();
         $x("//*[text()=\"Запланировать\"]").click();
-        $("[data-test-id='success-notification']").shouldBe(Condition.visible, ofSeconds(15));
-        //$("[class=\"notification__content\"]").shouldHave(text("Встреча успешно забронирована на " + firstMeeting));
+        $("[data-test-id='success-notification']").shouldBe(visible, ofSeconds(15));
+        $("[data-test-id='success-notification']>.notification__content").shouldHave(text("Встреча успешно запланирована на " + firstMeeting));
         $("[placeholder=\"Дата встречи\"]").sendKeys(Keys.chord(Keys.CONTROL, "a") + Keys.BACK_SPACE);
         $("[placeholder=\"Дата встречи\"]").setValue(secondMeeting);
         $x("//*[text()=\"Запланировать\"]").click();
-        $("[data-test-id='success-notification']").shouldBe(Condition.visible);
-        //$("[data-test-id='success-notification'].notification__content").shouldHave(text("У вас уже запланирована встреча на другую дату. Перепланировать?"));
+        $("[data-test-id='replan-notification']").shouldBe(visible);
+        $("[data-test-id='replan-notification']>.notification__content").shouldHave(text("У вас уже запланирована встреча на другую дату. Перепланировать?"));
         $x("//*[text()=\"Перепланировать\"]").click();
-        //$("[data-test-id='replan-notification']").shouldBe(Condition.visible);
-        //$("[data-test-id='replan-notification']>.notification__content").shouldHave(exactText("Встреча успешно забронирована на " + secondMeeting));
+        $("[data-test-id='success-notification']").shouldBe(Condition.visible);
+        $("[data-test-id='success-notification']>.notification__content").shouldHave(exactText("Встреча успешно запланирована на " + secondMeeting));
 
-
-
-        // !!!!!!!!!
-        // Сделал игнорируемымми строки:39,44,46,47 т.к. SUT не находит эти элементы по локатору.ISSUES прилагаю.
     }
-
 
 }
